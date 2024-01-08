198e6d1baf04b6b6cee64cb431220760c2f1853a
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-08 19:01:36 +0200

    esim2 valmis

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Person
+    {
+        private string fname;
+        public void setFname(string value)
+        {
+            fname = value;
+        }
+        public string getFname()
+        {
+            return fname;
+        }
+        public void showPerson()
+        {
+            Console.WriteLine("Hei olen " + fname);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person objectPerson = new Person();
+            objectPerson.setFname("Teppo");
+            //Console.WriteLine("Nimi = "+objectPerson.getFname());  
+            objectPerson.showPerson();
+
+            Person2 objectPerson2 = new Person2();
+            objectPerson2.Fname = "Liisa";
+            Console.WriteLine("Nimi="+objectPerson2.Fname);
+        }
+    }
+}
